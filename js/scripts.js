e2f69fc96af3f23e41ea7ab9150b1394675d453a
KHOIRUL_UMAM <khoirulumam1217@gmail.com>
2026-01-12 17:16:15 +0700

    Upload portofolio pertama saya

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+/*!
+* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Fungsi untuk mengecilkan navbar saat scroll
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Jalankan fungsi saat halaman dimuat
+    navbarShrink();
+
+    // Jalankan fungsi saat halaman di-scroll
+    document.addEventListener('scroll', navbarShrink);
+
+    // Aktifkan Bootstrap ScrollSpy (agar menu aktif berubah sesuai posisi scroll)
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse navbar otomatis saat menu diklik (tampilan mobile)
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Aktifkan Plugin SimpleLightbox (Untuk galeri foto pop-up)
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
